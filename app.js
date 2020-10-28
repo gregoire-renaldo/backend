587801ab25d939bf623ef282f4456a1914373fa7
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-10-28 18:04:59 +0100

    auth ok

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const stuffRoutes = require('./routes/stuff.js');
+const userRoutes = require('./routes/user');
 
 const app = express();
 
@@ -24,5 +25,6 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
