8debbc1bac08c5c649c99ec875122bcd035da436
gregoire renaldo <gregoire.renaldo@gmail.com>
2020-10-29 10:57:29 +0100

    upload images ok

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const stuffRoutes = require('./routes/stuff.js');
 const userRoutes = require('./routes/user');
@@ -23,6 +24,8 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
